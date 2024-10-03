3392d7f4a70546103a7e1dcba0e70e8ec21723a1
Dyego Alexandre Eugenio <dyegoe@gmail.com>
2024-10-03 13:54:35 +0300

    restrict versions

--- terraform.tf
+++ terraform.tf
@@ -1,13 +1,3 @@
 terraform {
   required_version = ">= 1.3.0"
-  required_providers {
-    proxmox = {
-      source  = "bpg/proxmox"
-      version = "~> 0.49.0"
-    }
-    cloudflare = {
-      source  = "cloudflare/cloudflare"
-      version = "~> 4.38.0"
-    }
-  }
 }
