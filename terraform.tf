6aa9ec09d52677d167d88d09f4f5f15401c44664
Dyego Alexandre Eugenio <dyegoe@gmail.com>
2024-10-03 13:23:54 +0300

    bump cloudflare version

--- terraform.tf
+++ terraform.tf
@@ -1,3 +1,13 @@
 terraform {
   required_version = ">= 1.3.0"
+  required_providers {
+    proxmox = {
+      source  = "bpg/proxmox"
+      version = ">= 0.49.0"
+    }
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = ">= 4.39.0"
+    }
+  }
 }
